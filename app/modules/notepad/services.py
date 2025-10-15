87fe49569654a58fdbe7068c50779425b5afb655
Samuel Granado Oliva <samgraoli@alum.us.es>
2025-10-15 21:33:44 +0200

    fix: fixed models file which made practica1 not work properly

--- app/modules/notepad/services.py
+++ app/modules/notepad/services.py
@@ -1,7 +1,6 @@
 from app.modules.notepad.repositories import NotepadRepository
 from core.services.BaseService import BaseService
 
-
 class NotepadService(BaseService):
     def __init__(self):
         super().__init__(NotepadRepository())
